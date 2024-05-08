c389eaec40fdc870ac95f4a00685c40d092e8503
filippo.labate <167630886+ProWebsiteTester@users.noreply.github.com>
2024-05-08 08:17:25 +0000

    i v

--- ex11.c
+++ ex11.c
@@ -1,1 +1,34 @@
-numero primo
+#include <stdio.h>
+
+int main()
+{
+    int a; //creazione variabili
+    int b = a - 1;
+    int c = 0;
+    
+    printf("Inserisci un numero \n"); //richiesta all'utente
+    scanf("%d", &a); //inserisci variabile a
+
+
+
+    while (b > 1)
+    {
+        if (a%b == 0)
+        {
+           c = c + 1; 
+        }
+        
+        b = b - 1;    
+    }
+    
+        if (c == 0)
+        {
+            printf("Il numero %d è primo", a);
+        }
+
+        else
+        {
+            printf("Il numero %d non è primo", a);
+        }
+       
+}
