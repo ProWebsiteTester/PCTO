57f978d40bc1b1dad8cd4a04334c5b1d829de587
filippo.labate <167630886+ProWebsiteTester@users.noreply.github.com>
2024-05-09 11:00:29 +0000

    quarto giorno

--- testplace.c
+++ testplace.c
@@ -1,17 +1,25 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include <stdio.h> //solita libreria
+#include <string.h> //libreria per le stringhe negli if facilitati
+#include <ctype.h> //per poter semplificare gli if mettendo tutto in minuscole
+#include <unistd.h>
 
 int main()
 {
-    // The URL you want to curl
-    const char *url = "parrot.live";
+    char inizio[25];
+    float prezzo;
+    char add[200];
+    char nome[50];
+    char indirizzo[500];
+    int si = 0;
+    char risposta;
 
-    // Construct the curl command
-    char command[1000];
-    sprintf(command, "curl %s", url);
+    while (si == 0)
+    {
+        printf("vuoi ordinare altro?\n");
+        scaf(" %[^\n]s", risposta);
 
-    // Execute the curl command
-    system(command);
+    
 
-    return 0;
+    }
+    
 }
