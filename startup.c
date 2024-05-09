9dc7918bc6cd13b1f513c39a041a229765cc87b1
filippo.labate <167630886+ProWebsiteTester@users.noreply.github.com>
2024-05-09 14:55:12 +0000

    quarto giorno

--- startup.c
+++ startup.c
@@ -1,78 +1,153 @@
 #include <stdio.h> //solita libreria
 #include <string.h> //libreria per le stringhe negli if facilitati
 #include <ctype.h> //per poter semplificare gli if mettendo tutto in minuscole
-#include <unistd.h>
+#include <unistd.h> //per comando sleep
+
+
+int si = 0;
+
 
 int main()
 {
-    char inizio[25];
-    float prezzo;
-    char add[200];
-    char nome[50];
-    char indirizzo[500];
 
-    printf("Benvenuto a SkyPlates\n\nCosa vorresti ordinare?\n");
+    printf("Benvenuto a SkyPlates\n\n");
     sleep(1);
-    printf("\nPer adesso offriamo solo:\nPizza\nYogurt\nBurger\n\n");
-    scanf(" %[^\n]", inizio);
 
-    for (int i = 0; inizio[i]; i++)
+    while (si == 0)
     {
-        inizio[i] = tolower(inizio[i]);
-    }
+        char inizio[25];
+        float prezzo = 0;
+        char add[200];
+        char nome[50];
+        char indirizzo[500];
 
+        char risposta[3];
+        char size[2];
 
-    if (strcmp(inizio, "pizza") == 0 || strcmp(inizio, "burger") == 0 || strcmp(inizio, "yogurt") == 0) 
-    {
-        if (strcmp(inizio, "pizza") == 0)
+        printf("Cosa vorresti ordinare?\n");
+        printf("\nPer adesso offriamo solo:\nPizza\nYogurt\nBurger\n\n");
+        scanf(" %[^\n]", inizio);
+
+        for (int i = 0; inizio[i]; i++)
         {
-            printf("\nCosa vorresti mettere sulla tua pizza?\n");
-            scanf(" %[^\n]s", add);
-            printf("La tua pizza con %s", add);
-            prezzo = 15.50;
-            printf(" costerà  €%.2f", prezzo);
+            inizio[i] = tolower(inizio[i]);
         }
-    
-        else if (strcmp(inizio, "yogurt") == 0)
+        if (strcmp(inizio, "pizza") == 0 || strcmp(inizio, "burger") == 0 || strcmp(inizio, "yogurt") == 0) 
         {
-            printf("\nCosa vorresti mettere sul tuo yogurt?\n");
-            scanf(" %[^\n]s", add);
-            printf("Il tuo yogurt con %s", add);
-            prezzo = 5.50;
-            printf(" costerà  €%.2f", prezzo);
-        }
+            if (strcmp(inizio, "pizza") == 0)
+            {
+                printf("\nCosa vorresti mettere sulla tua pizza?\n");
+                scanf(" %[^\n]s", add);
+                printf("La tua pizza con %s", add);
+                prezzo = 15.50;
+                printf(" costerà  €%.2f", prezzo);
+            }
+        
+            else if (strcmp(inizio, "yogurt") == 0)
+            {   
+                printf("Che quantità preferisci? \nS(€4.50), M(€5.00) o L(€6.00)?\n");
+                scanf(" %[^\n]s", size);
 
-        else if (strcmp(inizio, "burger") == 0)
-        {
-            printf("\nCosa vorresti mettere nel tuo burger?\n");
-            scanf(" %[^\n]s", add);
-            printf("Il tuo burger con %s", add);
-            prezzo = 17.50;
-            printf(" costerà  €%.2f", prezzo);
+                for (int i = 0; size[i]; i++)
+                {
+                    size[i] = tolower(size[i]);
+                }
+
+                if (strcmp (size, "s") == 0)
+                {
+                    prezzo = prezzo + 4.50;
+                }
+
+                if (strcmp (size, "m") == 0)
+                {
+                    prezzo = prezzo + 5.00;
+                }
+
+                if (strcmp (size, "l") == 0)
+                {
+                    prezzo = prezzo + 6.00;
+                }
+                
+                printf("\nCosa vorresti mettere sul tuo yogurt?\n");
+                scanf(" %[^\n]s", add);
+                printf("Il tuo yogurt con %s", add);
+
+                printf(" costerà  €%.2f", prezzo);
+            }
+
+            else if (strcmp(inizio, "burger") == 0)
+            {
+                printf("Che dimensione preferisci? \nS(€11.50), M(€13.00) o L(€15.00)?\n");
+                scanf(" %[^\n]s", size);
+
+                for (int i = 0; size[i]; i++)
+                {
+                    size[i] = tolower(size[i]);
+                }
+
+                if (strcmp (size, "s") == 0)
+                {
+                    prezzo = prezzo + 11.50;
+                }
+
+                if (strcmp (size, "m") == 0)
+                {
+                    prezzo = prezzo + 13.00;
+                }
+
+                if (strcmp (size, "l") == 0)
+                {
+                    prezzo = prezzo + 15.00;
+                }
+
+                printf("\nCosa vorresti mettere nel tuo burger?\n");
+                scanf(" %[^\n]s", add);
+                printf("Il tuo burger con %s", add);
+                printf(" costerà  €%.2f", prezzo);
+            }
+            
+            printf("\n\nA quale indirizzo dobbiamo consegnare %s", inizio);
+            printf(" con %s?\n", add);
+            scanf(" %[^\n]s", indirizzo);
+            printf("Come ti chiami?\n");
+            scanf(" %[^\n]s", nome);
+
+            if (strcmp(inizio, "pizza") == 0)
+            {
+                printf("\nLa tua pizza arriverà al volo a %s \n\n", indirizzo);
+                printf("La preghiamo di assicurarsi che %s sia presente per la consegna\n", nome);
+            }
+
+            if (strcmp(inizio, "burger") == 0 || strcmp(inizio, "yogurt") == 0)
+            {
+                printf("\nIl tuo %s arriverà ", inizio);
+                printf("a %s\n\n", indirizzo);
+                printf("La preghiamo di assicurarsi che %s sia presente per la consegna\n", nome);
+            }
         }
         
-        printf("\n\nA quale indirizzo dobbiamo consegnare %s", inizio);
-        printf(" con %s?\n", add);
-        scanf(" %[^\n]s", indirizzo);
-        printf("Come ti chiami?\n");
-        scanf(" %[^\n]s", nome);
-
-        if (strcmp(inizio, "pizza") == 0)
+        else
         {
-            printf("\nLa tua pizza arriverà al volo a %s \n\n", indirizzo);
-            printf("La preghiamo di assicurarsi che %s sia presente per la consegna\n", nome);
+            printf("Per adesso %s non è disponibile\n", inizio);
         }
-
-        if (strcmp(inizio, "burger") == 0 || strcmp(inizio, "yogurt") == 0)
+        printf("Vuoi ordinare altro? (si/no)\n");
+        scanf(" %[^\n]s", risposta);
+        
+        for (int i = 0; risposta[i]; i++)
         {
-            printf("\nIl tuo %s arriverà al volo ", inizio);
-            printf("a %s\n\n", indirizzo);
-            printf("La preghiamo di assicurarsi che %s sia presente per la consegna\n", nome);
+            risposta[i] = tolower(risposta[i]);
         }
-    }
-    
-    else
-    {
-        printf("Per adesso %s non è disponibile, seleziona un'altra opzione al riavvio del programma\n", inizio);
+        
+        if (strcmp(risposta, "si") == 0)
+        {
+            si = 0;
+            printf("Il tuo nuovo ordine verà aggiunto a quello precedente\n\n");
+        }
+        
+        else
+        {
+            si = 1;
+            printf("Grazie per aver scelto Skyplates\n\n");
+        }   
     }
 }
--- testplace.c
+++ testplace.c
@@ -1,7 +1,7 @@
 #include <stdio.h> //solita libreria
 #include <string.h> //libreria per le stringhe negli if facilitati
 #include <ctype.h> //per poter semplificare gli if mettendo tutto in minuscole
-#include <unistd.h>
+#include <unistd.h> //per comando sleep
 
 int main()
 {
@@ -11,15 +11,34 @@ int main()
     char nome[50];
     char indirizzo[500];
     int si = 0;
-    char risposta;
+    char risposta[2];
+
+    printf("Benvenuto a SkyPlates\n\n");
+    sleep(1);
 
     while (si == 0)
     {
+        printf("Cosa vorresti ordinare?\n");
+        printf("\nPer adesso offriamo solo:\nPizza\nYogurt\nBurger\n\n");
+
         printf("vuoi ordinare altro?\n");
-        scaf(" %[^\n]s", risposta);
-
-    
-
+        scanf(" %[^\n]s", risposta);
+        
+        for (int i = 0; risposta[i]; i++)
+        {
+            risposta[i] = tolower(risposta[i]);
+        }
+        
+        if (strcmp(risposta, "no") == 0)
+        {
+            si = 1;
+            printf("Grazie per aver scelto SkyPlate\n\n");
+        }
+        
+        else
+        {
+            si = 0;
+            printf("Il tuo nuovo ordine verà aggiunto a quello precedente\n\n");
+        }   
     }
-    
 }
